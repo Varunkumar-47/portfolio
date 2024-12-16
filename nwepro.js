44a81186d832eab306549ab687b1cd61d5c7a97c
Varunkumar-47 <varunkumarn47@gmail.com>
2024-12-16 12:34:00 +0530

    First Commit

---
+++ nwepro.js
@@ -0,0 +1,65 @@
+// toggle ioc navbar
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuicon.onclick = () =>  {
+    menuicon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+// scroll
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a ');
+
+window.onscroll =()=>{
+    sections.forEach(sec =>{
+        let top =window.scrollY;
+        let offset =  sec.offsetTop-150;
+        let height =sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top>=offset && top < offset+height){
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            })
+        }
+
+    });
+    // stlcky navbar
+let header =document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY>100);
+
+
+
+
+// both
+menuicon.classList.toggle('bx-x');
+navbar.classList.toggle('active');
+};
+
+// scroll revale
+
+ScrollReveal({
+    //  reset: true,
+     distance:'80px',
+     duration:2000,delay:200
+     });
+
+  ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+  ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'bottom' });
+  ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left' });
+  ScrollReveal().reveal('.home-content p, .about-content', { origin:'right' });
+// mutlpe texrt
+
+const typed = new typed('.multiple-text',{
+    Strings:['python full stock developer','wed developer','</>'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
+
+
+
+
